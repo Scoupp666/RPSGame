bb8c7f4bce7c7b74eb986a88b31125fa7be45c94
Scouperopps666 <143419721+Scouperopps@users.noreply.github.com>
2024-11-27 00:09:11 -0600

    RPS1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import RockPaperScissors from "./Components/rockPapScissor";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RockPaperScissors />
+    </div>
+  );
+}
+
+export default App;
+
+
